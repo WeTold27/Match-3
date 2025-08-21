575a12af0f10299a764adc94e276a5b72f67b0f7
WeTold27 <lebedkin.n.v@mail.ru>
2025-08-21 22:51:38 +0500

    Update game scene and add Preload scene

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
 
 import Phaser from "phaser";
-import GameScene from "./scenes/GameScene";
 
+import GameScene from "./scenes/GameScene";
+import PreloadScene from './scenes/Preload';
+
+debugger
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 730,
   height: 730, 
   backgroundColor: "#222222",
+  scene: [PreloadScene, GameScene],
   physics: {
     default: 'arcade'
   },
-  
-  scene: [ GameScene]
 };
 
 
-new Phaser.Game(config);
+const game = new Phaser.Game(config);
+
+window.onload = () => {
+  const canvas = game.canvas
+
+  canvas.style.position = "absolute";
+  canvas.style.left = "50%";
+  canvas.style.top = "50%";
+  canvas.style.transform = "translate(-50%, -50%)";
+
+  document.body.style.background = "#111";
+  document.body.style.margin = "0";
+  document.body.style.overflow = "hidden";
+}
